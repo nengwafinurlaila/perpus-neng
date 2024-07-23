b683f7b4e100d07471e002be76330c216e9af781
nengwafinurlaila <nengwapii72@gmail.com>
2024-07-23 11:07:55 +0700

    Add files via upload
    new perpus

---
+++ pages/rincian buku/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container shadow p-2 mt-3">
+    <div class="row mt-4">
+      <div class="col-4">
+        <img src="~assets/img/buku1.jpg" class="cover">
+      </div>
+      <div class="col-4">
+        <p>judul :BAWANG MERAH & BAWANG PUTIH</p>
+        <p> penerbit :Lingkar Media</p>
+        <p> tahun terbit :2009</p>
+      </div>
+    </div>
+    <div class="row">
+      <p class="mt-5">Di sebuah desa, tinggalah seorang janda yang hidup dengan dua anak perempuannya yang memiliki wajah menawan, yaitu Bawang Merah dan Bawang Putih. Ayah kandung Bawang Putih yang juga suami dari ibu Bawang Merah telah meninggal dunia, jadi Bawang Putih adalah saudara tiri dari Bawang Merah.
+Bawang Merah dan Bawang Putih memiliki sifat karakter serta kepribadian yang berbeda. Bawang Putih memiliki sifat yang rajin, baik hati, jujur, dan rendah hati. Sementara, Bawang Merah memiliki sifat yang malas, sombong, iri hati.</p>
+    </div>
+  </div>
+</template>
+<nuxt-link to="http://localhost:3000/buku">
+
+  <button type="submit" class="btn btn-secondary btn-lg rounded-5 px-5">LANJUT</button>
+</nuxt-link>
+<style scoped>
+
+</style>
